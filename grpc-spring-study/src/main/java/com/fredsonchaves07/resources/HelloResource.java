cae0ddc6c1b7d4e4ef617939ab55d3e8661f8df7
Fredson <fredsonchaves07@gmail.com>
2023-01-18 17:28:27 -0300

    feat: configuracao da estrutura do arquivo proto

---
+++ grpc-spring-study/src/main/java/com/fredsonchaves07/resources/HelloResource.java
@@ -0,0 +1,18 @@
+package com.fredsonchaves07.resources;
+
+import com.fredsonchaves07.HelloReq;
+import com.fredsonchaves07.HelloRes;
+import com.fredsonchaves07.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloResource extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void hello(HelloReq request, StreamObserver<HelloRes> responseObserver) {
+        var response = HelloRes.newBuilder().setMessage(request.getMessage()).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
