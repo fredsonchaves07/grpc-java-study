d06816c145397b826e01e10527a46aad750d71ee
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-03-27 16:42:18 -0300

    feat: alterando o arquivo protofile

--- grpc-cliente-example/src/main/java/com/fredsonchaves07/ProductClientApp.java
+++ grpc-cliente-example/src/main/java/com/fredsonchaves07/ProductClientApp.java
@@ -7,8 +7,17 @@ public class ProductClientApp {
 
     public static void main(String[] args) {
         ManagedChannel channel = ManagedChannelBuilder
-                .forAddress("localhost", 50051)
+                .forAddress("localhost", 9090)
                 .usePlaintext()
                 .build();
+        ProductServiceGrpc.ProductServiceBlockingStub stub =
+                ProductServiceGrpc.newBlockingStub(channel);
+        ProductRequest productRequest = ProductRequest.newBuilder()
+                .setName("Product 1")
+                .setPrice(10)
+                .setQuantityInStock(100)
+                .build();
+        ProductResponse productResponse = stub.create(productRequest);
+        channel.shutdown();
     }
 }
